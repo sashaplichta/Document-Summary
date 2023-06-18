96351b267a04bf03ae0bf20481697695af6df284
sam <112935537+smh-94@users.noreply.github.com>
2023-06-17 22:59:24 -0700

    comments

--- post_db.py
+++ post_db.py
@@ -2,11 +2,12 @@ import pymongo
 #samplesamplesample
 username = 'John Wick'
 data = {
+    #*** is missing a question/answer and relations
     #data structure is 
     #pdf_id : [timespent, associated_tags, quiz_id
     #quiz_id : {question_num : boolean}
     'pdf01' : ['1:00','science',['quiz_01','quiz_02']],
-    'pdf02' : ['1:40','biology'],
+    'pdf02' : ['1:40','biology',['quiz_06','quiz_08']],
     'quiz_01' : {'question1': True,'question2': False, 'question3': True, 'score' : 90},
     'quiz_02' : {'question1': False,'question2': True, 'question3': False, 'score' : 95}
     }
@@ -20,8 +21,7 @@ def insertData(data):
         print("Duplicate found. Data not inserted")
     else:
         col.insert_one(data)
-
-def dbcall(username) -> dict:
+def dbcall(username: str) -> dict:
     #retrieves data using username
     key_to_search = username
     query = {key_to_search: {"$exists": True}}
